d454dafff5ba52c94e17ebb540b66f58ed8c9db6
Yamunika Chintapanti <199625828+yamunikac@users.noreply.github.com>
2026-02-09 21:14:31 +0530

    Refactor calculator input and output formatting

--- Task_1_Calculator/Calculator.py
+++ Task_1_Calculator/Calculator.py
@@ -9,30 +9,23 @@ print("====================================")
 while True:
     num1 = float(input("\nEnter the first number: "))
     num2 = float(input("Enter the second number: "))
-  
     print("\nChoose an operation:")
     print("Addition (+)")
     print("Subtraction (-)")
     print("Multiplication (*)")
     print("Division (/)")
-
     choice = input("Enter the operation symbol (+, -, *, /): ")
-
     if choice == "+":
         print("Result:", num1 + num2)
-
     elif choice == "-":
         print("Result:", num1 - num2)
-
     elif choice == "*":
         print("Result:", num1 * num2)
-
     elif choice == "/":
         if num2 != 0:
             print("Result:", num1 / num2)
         else:
             print("Error: Division by zero is not allowed")
-
     else:
         print("Invalid operation! Please choose a valid symbol.")
 
